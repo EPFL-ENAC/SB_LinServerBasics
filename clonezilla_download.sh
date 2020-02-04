2810c9192aacf31b9ebf70a366e396015a3739bb
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-02-04 11:12:10 +0100

    [ADD](clonezilla_download.sh)→ automated script to download current Clonezilla iso at the right plac

---
+++ clonezilla_download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CURRENT_RELEASE="20191024-eoan"
+
+FILEMANE="clonezilla-live-${CURRENT_RELEASE}-amd64.iso"
+URL="https://netcologne.dl.sourceforge.net/project/clonezilla/clonezilla_live_alternative/${CURRENT_RELEASE}/${FILEMANE}"
+
+wget ${URL} -O /clone_sys/${FILEMANE}
+rm -f /clone_sys/clonezilla.iso
+ln -s ${FILEMANE} /clone_sys/clonezilla.iso
