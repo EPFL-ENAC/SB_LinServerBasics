25dc24b966c244184647af509fa2300e7cae9862
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-03-23 14:49:34 +0100

    [FIX](clonezilla_download.sh)→ mkdir -p /clone_sys

--- clonezilla_download.sh
+++ clonezilla_download.sh
@@ -5,6 +5,8 @@ CURRENT_RELEASE="20210127-groovy"
 FILEMANE="clonezilla-live-${CURRENT_RELEASE}-amd64.iso"
 URL="https://netcologne.dl.sourceforge.net/project/clonezilla/clonezilla_live_alternative/${CURRENT_RELEASE}/${FILEMANE}"
 
+mkdir -p /clone_sys
+
 wget ${URL} -O /clone_sys/${FILEMANE}
 rm -f /clone_sys/clonezilla.iso
 ln -s ${FILEMANE} /clone_sys/clonezilla.iso
